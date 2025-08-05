b1db13ec0d2f2436ff6a64e36f4844c52cd9b0a7
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-08-05 14:29:34 -0700

    :zap: re-enable main renderer

--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -376,11 +376,11 @@ export default class WebGPURenderer {
       );
 
       // Render
-      // passEncoder.setPipeline(renderPipeline);
-      // passEncoder.setBindGroup(0, uniformBindGroup);
-      // passEncoder.setVertexBuffer(0, vertexBuffer);
-      // passEncoder.setIndexBuffer(indexBuffer, "uint16");
-      // passEncoder.drawIndexed(indices.length, instanceCount);
+      passEncoder.setPipeline(renderPipeline);
+      passEncoder.setBindGroup(0, uniformBindGroup);
+      passEncoder.setVertexBuffer(0, vertexBuffer);
+      passEncoder.setIndexBuffer(indexBuffer, "uint16");
+      passEncoder.drawIndexed(indices.length, instanceCount);
 
       passEncoder.end();
       // Finish rendering
